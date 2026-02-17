ebfeaefb9ca6a2eb079f9cca09d4543973735f0e
Aditi-kod <ad2019ti@gmail.com>
2026-02-17 18:54:17 +0530

    Update CORS origin to use HTTPS

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-        origin: "http://adorewebapp.vercel.app",
+        origin: "https://adorewebapp.vercel.app",
         credentials: true,
     })
 );
